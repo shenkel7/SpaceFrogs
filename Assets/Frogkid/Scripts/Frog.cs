099c2408d357c19f50ba699030bbf39349b6b892
Kelly Shen <kellyshenfrog@gmail.com>
2021-12-17 18:00:00 -0600

    core game frame implemented

---
+++ Assets/Frogkid/Scripts/Frog.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FROGKID
+{
+
+    public class Frog : MonoBehaviour
+    {
+        Rigidbody2D body;
+
+        float horizontal;
+        float vertical;
+
+        public float runSpeed = 20.0f;
+
+        [SerializeField]
+        SeekMinigame seekMinigame;
+
+        void Start()
+        {
+            body = GetComponent<Rigidbody2D>();
+            body.freezeRotation = true;
+        }
+
+        void Update()
+        {
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
+        }
+
+        private void FixedUpdate()
+        {
+            body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Planet planet = collision.gameObject.GetComponent<Planet>();
+            if (planet != null)
+            {
+                if (planet.getIsHome())
+                {
+                    seekMinigame.doWin();
+                } else
+                {
+                    seekMinigame.doLose();
+                }
+            }
+                
+        }
+
+    }
+
+}
---
+++ Assets/Frogkid/Scripts/SeekMinigame.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+namespace FROGKID
+{
+    public class SeekMinigame : MonoBehaviour
+    {
+
+        public Text UIText;
+        public string startText;
+        public string winText;
+
+        [SerializeField]
+        private GameObject frog = null;
+
+        [SerializeField]
+        private List<Planet> planets = null;
+
+        private List<Planet> selectedPlanets;
+
+        [SerializeField]
+        private List<int> planetSelector;
+
+        int homePlanet;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            UIText.text = startText;
+            MinigameManager.Instance.minigame.gameWin = false;
+            selectedPlanets = new List<Planet>();
+            int numPlanets = planets.Count;
+
+
+            homePlanet = Random.Range(0, 3);
+
+            for(int i = 0; i < 3; i++)
+            {
+                if(i != homePlanet)
+                {
+                    // select a sprite at random
+                    int index = Random.Range(0, planetSelector.Count);
+                    int p = planetSelector[index];
+                    planetSelector.RemoveAt(index);
+                    planets[i].setPlanetNum(p);
+                } else
+                {
+                    // set home planet sprite
+                    planets[i].setPlanetNum(0);
+                    Debug.Log(i);
+                }
+            }
+
+            //// select 3 random planets
+            //for(int i = 0; i < 3 && i < numPlanets; i++)
+            //{
+            //    int index = Random.Range(0, planets.Count);
+            //    GameObject p = planets[index];
+            //    selectedPlanets.Add(p);
+            //    planets.RemoveAt(index);
+            //}
+
+
+            Debug.Log(selectedPlanets.Count);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void doWin()
+        {
+            Debug.Log("win");
+            if (!MinigameManager.Instance.minigame.gameWin)
+            {
+                MinigameManager.Instance.minigame.gameWin = true;
+                UIText.text = winText;
+                MinigameManager.Instance.PlaySound("win");
+            }
+        }
+
+        public void doLose()
+        {
+            Debug.Log("lose");
+        }
+    }
+}
