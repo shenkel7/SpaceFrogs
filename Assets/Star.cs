0bb44a61eb0f72341c7df64be56ead569ded5bdc
Kelly Shen <kellyshenfrog@gmail.com>
2021-12-18 10:57:34 -0600

    add starts

---
+++ Assets/Star.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+
+
+    [SerializeField]
+    Transform transform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0, 0, 10 * Time.deltaTime);
+    }
+}
